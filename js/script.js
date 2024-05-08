a95889e864dbae4342801074c4ad5b57990ad729
Michael Lowry <michael.lowry64@gmail.com>
2024-05-08 15:38:17 -0400

    Got a basic grid together
     (I am using flexbox instead of grid on purpose)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++)
+{
+    for (let j = 0; j < 16; j++)
+    {
+        let newDiv = document.createElement("div");
+        newDiv.style.width = `${(1 / 16) * 100}%`;
+        newDiv.style.height = `${(1 / 16) * 100}%`;
+        newDiv.innerText = `${j+1}`;
+        container.appendChild(newDiv);
+        
+    }
+    
+}
