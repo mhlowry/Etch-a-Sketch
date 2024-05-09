997ecd4040a4c4b01d8e8815e6badb1d8d0bbb04
Michael Lowry <michael.lowry64@gmail.com>
2024-05-09 09:58:44 -0400

    Change size input to require a button press
    Helps big-oh

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 let container = document.querySelector(".container");
+let gridSizeText = document.querySelector("h3");
+let slider = document.querySelector("input");
+let submit = document.querySelector("button");
 
-for (let i = 0; i < 16; i++)
-{
-    for (let j = 0; j < 16; j++)
-    {
-        let newDiv = document.createElement("div");
-        newDiv.style.width = `${(1 / 16) * 100}%`;
-        newDiv.style.height = `${(1 / 16) * 100}%`;
-        newDiv.innerText = `${j+1}`; // for debugging
+slider.addEventListener("input", (e) => {
+    gridSizeText.innerText = `${slider.value}x${slider.value}`;
+});
 
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.style.backgroundColor = 'black';
-        });
+submit.addEventListener("pointerdown", () => {
+    container.innerHTML = '';
+    createGrid(slider.value);
+});
 
-        container.appendChild(newDiv); 
-    }
-    
+function createGrid(userInput=16) {
+    for (let i = 0; i < userInput; i++)
+        for (let j = 0; j < userInput; j++)
+        {
+            let newDiv = document.createElement("div");
+            newDiv.style.width = `${(1 / userInput) * 100}%`;
+            newDiv.style.height = `${(1 / userInput) * 100}%`;
+
+            newDiv.addEventListener("mouseover", () => {
+                newDiv.style.backgroundColor = 'black';
+            });
+
+            container.appendChild(newDiv); 
+        }
+
 }
+
+createGrid();
